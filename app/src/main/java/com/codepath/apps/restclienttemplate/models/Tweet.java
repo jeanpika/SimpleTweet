e529ed2b4db3fd8264a462c0eea05f16920e4a6f
Kail Jean Pierre <spakimimi@gmail.com>
2021-02-17 21:53:48 -0500

    Update tweet 2

--- app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
@@ -20,6 +20,9 @@ public class Tweet  {
     public long id;
     public User user;
 
+    // empty constructor needed by the Parceler library
+    public Tweet(){}
+
     public static Tweet fromJson(JSONObject jsonObject) throws JSONException {
         Tweet tweet = new Tweet();
         tweet.body = jsonObject.getString("text");
